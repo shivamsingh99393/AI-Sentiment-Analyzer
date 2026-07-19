8525e7075517b3e3a718b54921dd46f26f6671a8
shivamsingh99393 <shivamsingh99393@gmail.com>
2026-07-19 14:51:04 +0530

    Added review history using MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from models.sentiment_model import analyze_sentiment
+from database.mongo import reviews
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -12,7 +14,25 @@ def home():
         review = request.form["review"]
         result = analyze_sentiment(review)
 
+        reviews.insert_one({
+            "review": review,
+            "sentiment": result[0]["label"],
+            "confidence": round(result[0]["score"] * 100, 2)
+        })
+
     return render_template("index.html", result=result)
+
+
+@app.route("/history")
+def history():
+
+    all_reviews = list(reviews.find())
+
+    return render_template(
+        "history.html",
+        reviews=all_reviews
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
